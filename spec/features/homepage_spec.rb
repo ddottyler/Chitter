590c55192082ac5f336d1ef1bbbc37e422131858
Dan T <dp.tyler@hotmail.co.uk>
2021-04-17 00:58:04 +0100

    Some issues with flash notices and deleting comments needs work. Focusing on styling next. Also need to add usernames and add usernames to each peep.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -23,3 +23,16 @@ feature 'Viewing peeps' do
     expect(page).to have_content "This is a third test peep"
   end
 end
+
+feature 'signing in and out options' do 
+  scenario 'user can see sign in option' do 
+    visit '/'
+    expect(page).to have_button 'Sign in'
+  end
+
+  scenario 'user can see sign up option' do 
+    visit '/'
+    expect(page).to have_button 'Sign up'
+  end
+  
+end
