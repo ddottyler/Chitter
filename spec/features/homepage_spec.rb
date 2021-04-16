cc2cd95d122729c1a9646c22c8b6e19411ae6645
Dan T <dp.tyler@hotmail.co.uk>
2021-04-16 21:12:20 +0100

    Add peep button created and showing on homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,4 +3,9 @@ feature 'Viewing chitter' do
     visit('/')
     expect(page).to have_content "Welcome to Chitter"
   end
+
+  scenario 'a user can see a add peep button' do 
+    visit('/')
+    expect(page).to have_button "Add Peep"
+  end
 end
