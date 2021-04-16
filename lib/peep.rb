7e618762beaf7fb9be53fb9437c181d94c9bbb0e
Dan T <dp.tyler@hotmail.co.uk>
2021-04-16 22:31:25 +0100

    Peeps now showing the time they are created

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,24 +2,38 @@ require 'pg'
 
 class Peep
 
+  attr_reader :id, :peep, :time
+
+  def initialize(id:, peep:, time:)
+      @id = id 
+      @peep = peep
+      @time = time 
+  end
+
   def self.all
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_new_test')
     else
       connection = PG.connect(dbname: 'chitter_new')
     end
-    peeps = connection.exec('SELECT * FROM chitters;')
-    peeps.map { |peep| peep['peep'] }
+    result = connection.exec('SELECT * FROM chitters;')
+    result.map { |peep| 
+      Peep.new(
+        id: peep['id'], 
+        peep: peep['peep'], 
+        time: peep['time']
+      )}
   end
 
   def self.create(peep:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_new_test')
-    else
+  else 
       connection = PG.connect(dbname: 'chitter_new')
-    end
-
-    connection.exec("INSERT INTO chitters (peep) VALUES('#{peep}')")
+  end
+    time = Time.new.strftime("%d/%m/%Y %k:%M")
+    result = connection.exec("INSERT INTO chitters (peep, time) VALUES('#{peep}', '#{time}') RETURNING id, peep, time;")
+    Chitter.new(id: result[0]['id'], peep: result[0]['peep'], time: result[0]['time'])
   end
 
 end
--- spec/model/peeps_spec.rb
+++ spec/model/peeps_spec.rb
@@ -11,9 +11,9 @@ describe '.all' do
 
     peep = Peep.all
 
-    expect(peep).to include("This is a test peep")
-    expect(peep).to include("This is a second test peep")
-    expect(peep).to include("This is a third test peep")
+    expect(peep[0].peep).to include("This is a test peep")
+    expect(peep[1].peep).to include("This is a second test peep")
+    expect(peep[2].peep).to include("This is a third test peep")
   end
 end
 
@@ -21,6 +21,6 @@ describe '.create' do
   it 'creates a new peep' do
     Peep.create(peep: 'This is a test peep')
 
-    expect(Peep.all).to include 'This is a test peep'
+    expect(Peep.all[0].peep).to include 'This is a test peep'
   end
 end
