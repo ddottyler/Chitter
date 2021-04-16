a17c4970f6ddd54a4271c9de32562b0b3f6562ba
Dan T <dp.tyler@hotmail.co.uk>
2021-04-16 21:22:21 +0100

    Test database set up and logged in db migrations

---
+++ db/migrations/01_creating_peeps_table.sql
@@ -0,0 +1,5 @@
+-- Database - chitter_new
+CREATE TABLE chitters(id SERIAL PRIMARY KEY, "peep" VARCHAR(100), "time" VARCHAR);
+
+-- Database - chitter_new_test
+CREATE TABLE chitters(id SERIAL PRIMARY KEY, "peep" VARCHAR(100), "time" VARCHAR);
